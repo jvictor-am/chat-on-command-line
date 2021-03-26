0624ee4279799e9c7d455f85fdc256f5078ae915
jvictor-am <joaovictorengp@gmail.com>
2021-03-26 02:21:49 -0300

    Implements parse CliConfig

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
 import Events from 'events'
+import CliConfig from './src/cliConfig.js'
 import TerminalController from './src/terminalController.js'
+
+const [nodePath, filePath, ...commands] = process.argv
+
+const config = CliConfig.paserArguments(commands)
+console.log('config', config)
 
 const componentEmitter = new Events()
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
