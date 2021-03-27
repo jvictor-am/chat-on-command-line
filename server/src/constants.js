d4bf7fb8508791546d645da58c2bb31d5a9f065a
jvictor-am <joaovictorengp@gmail.com>
2021-03-26 21:42:36 -0300

    Connection stablished between socket server & client

---
+++ server/src/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+    event: {
+        MESSAGE: 'message',
+        NEW_USER_CONNECT: 'newUserConnected',
+        DISCONNECT_USER: 'disconnectUser',
+        UPDATE_USERS: 'updateUsers'
+    }
+}
